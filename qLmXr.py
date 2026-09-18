1937e1b217ea88c4f15323b0c5cda17276c0a756
micahbaron7-hash <micahbaron7@gmail.com>
2026-09-17 20:23:17 -0400

    Update qLmXr.py

--- qLmXr.py
+++ qLmXr.py
@@ -1,4 +1,4 @@
-```python
+
 from flask import Flask, request, render_template, jsonify
 import requests
 import os
@@ -639,4 +639,4 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=port
     )
-```
+
